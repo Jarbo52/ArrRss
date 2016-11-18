2e9d37565557a261807aa1c68b3883b937f24810
Jared Bass <jbass@stevens.edu>
2016-11-18 16:23:12 -0500

    Added feeds and feed popup

--- ArrRSSscript.js
+++ ArrRSSscript.js
@@ -1,19 +1,40 @@
-setTimeout(function()
-{        
-    var test = document.getElementById('rssHolder');
-    test.style.color="red";
-    test.style.display="inline";
-    console.log("done");
-}, 3000);
-console.log("hi");
+var currentLocation = "";
+var yqls=[];
+var names=[];
+var tags=[];
+
+function closeFeeds()
+{
+    setTimeout(function()
+    {
+        document.getElementById('rssHolder').style.display="none"; document.getElementById('listHolder').style.display="block";
+        document.getElementById('closeButton').style.display="none";
+        var rssFeed = document.getElementById('rss');
+        rssFeed.parentNode.removeChild(rssFeed);
+        
+    },500);
+    console.log("closing");
+}
 
 function rssRequest(callingElement)
 {
-    console.log(callingElement.id);
-    var newScript = document.createElement('script');
-    newScript.setAttribute('type','text/javascript');
-    newScript.setAttribute('src',callingElement.id);
-    document.getElementsByTagName('head')[0].appendChild(newScript);    
+    var holding = callingElement.innerHTML;
+    currentLocation=callingElement.className;
+    console.log(currentLocation);
+    setTimeout(function()
+    {
+        var newScript = document.createElement('script');
+        newScript.setAttribute('type','text/javascript');
+        newScript.setAttribute('src',callingElement.id);
+        newScript.setAttribute('id','rss');
+        document.getElementsByTagName('head')[0].appendChild(newScript); 
+        setTimeout(function()
+        {
+          callingElement.innerHTML=holding;        
+        },2500);
+    },5000);
+    
+    callingElement.innerHTML="Loading...";
 }
 
 function printRSS(response)
@@ -22,21 +43,49 @@ function printRSS(response)
     var rss = '';
     var article;
     var len = response.query.results.item.length;
-    console.log("lem: " + len);
+    if(len > 15)
+    {
+        len = 15;
+    }
+    console.log("len: " + len);
     for(var i = 0; i < len; i++)
     {
         article = response.query.results.item[i];
-        rss += '<li><a href = "' + article.link[0] + '">' + article.title + '</a></li>';
-        console.log("RSS: " + rss);
+        rss += '<li><a href = "' + article.link + '" target ="_blank">' + article.title + '</a></li>';
     }
     
-    console.log(rss);
     console.log("DONEE");
     
+    document.getElementById('closeButton').style.display="inline-block";
+    document.getElementById('listHolder').style.display="none";
     document.getElementById('rssHolder').innerHTML = rss;
+    document.getElementById('rssHolder').style.display="block";
 }
 
-function cbfunc(o)
+function searchButton()
 {
-    console.log("LEE");
+    document.getElementById('searchHolder').style.display="block";
+    document.getElementById('listHolder').style.display="none";
+}
+
+function searchFeeds()
+{
+    var searchData = document.getElementById('searchData');
+    var applicableFeeds = [];
+    if(searchData != "")
+    {
+        for(var i = 0; i < tags.length; i++)
+            if(tags[i].includes(searchData))
+                applicableFeeds.push(i); 
+        
+        for(var i = 0; i < applicableFeeds.length;i++)
+        {
+            var searcedFeed = document.createElement('div');
+            searcedFeed.setAttribute('class','rssFeed');
+            var searchedBody = document.createElement('div');
+            searchedBody.setAttribute('class','rssTitle');
+            var searchedData 
+        }
+    }    
+    alert("searched");
 }
---
+++ README.txt
@@ -0,0 +1,7 @@
+Team Members: Jordan Guzman, Connor Murphy, Jacob Fallin, Jared Bass
+
+We pledge our honor that we have abided by the Stevens Honor System.
+
+Main page done by Jared Bass.
+
+About page done by Jacob Fallin
